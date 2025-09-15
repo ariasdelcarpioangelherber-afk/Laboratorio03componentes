e9108d6cff672e6b0a03e86dabaab41b0e9168f2
Ángel Arias <ariasdelcarpioangelherber@gmail.com>
2025-09-14 20:43:44 -0500

    feat: Add ViewHolaCursresolveo

--- app/src/main/java/com/example/lab03kotlinariasdelcarpioangel/MainActivity.kt
+++ app/src/main/java/com/example/lab03kotlinariasdelcarpioangel/MainActivity.kt
@@ -45,23 +45,3 @@ fun GreetingPreview() {
     }
 }
 
-// Este código tiene varios errores a propósito
-fun ViewHolaCurso() {
-    Column(
-        modifier = Modifier
-            .fillMaxWith() // Error 1: Debería ser .fillMaxWidth()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Welcome to the Course!",
-            fontSize = 28.sp,
-            fontWeight = FontWeigh.Bold // Error 2: Debería ser FontWeight
-        )
-        Spacer(modifier = Modifier.heigh(16.dp)) // Error 3: Debería ser .height()
-        Text(
-            text = "Hello, Student!",
-            fontSize = 20.xD // Error 4: Debería ser 20.sp
-        )
-    }
-}
